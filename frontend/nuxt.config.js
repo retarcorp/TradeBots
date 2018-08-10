dbed402e3153c6cec7d753165082fae131dca6ec
ituxka <ituxka@gmail.com>
2018-08-10 10:54:55 +0300

    second setup

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,22 +1,35 @@
 module.exports = {
+  mode: "spa",
   /*
   ** Headers of the page
   */
   head: {
-    title: 'tradebots',
+    title: "tradebots",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'tradebots project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "tradebots project" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
+
+  /* 
+  ** Global css files
+  */
+  css: ["~/assets/css/grid.min.css", "~/assets/css/transitions/allpages.css"],
+
+  /*
+  ** Modules 
+  */
+
+  modules: ["@nuxtjs/axios"],
+  axios: {
+  },
+
   /*
   ** Build configuration
   */
@@ -24,16 +37,21 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
+  },
+
+  transition: {
+    name: 'page',
+    mode: 'out-in'
   }
-}
+};
 
