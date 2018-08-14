71a3dcd56988383a826398a08746e84d3bed454e
ituxka <ituxka@gmail.com>
2018-08-14 11:56:47 +0300

    upd

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -7,63 +7,7 @@ const store = () =>
   new Vuex.Store({
     state: {
       isAuthorized: true,
-      botsList: [
-        {
-          title: "!@#",
-          state: "1",
-          status: 0,
-          pair: {
-            from: "ETH",
-            to: "BNB"
-          },
-          orders: [
-            {
-              pair: {
-                from: "BTC",
-                to: "ETH"
-              },
-              state: 0,
-              amount: 0,
-              price: 0,
-              total: 0,
-              dateInfo: {
-                created: 1534157336853,
-                closed: null
-              },
-              data: "new order is created"
-            }
-          ],
-          currentOrder: {},
-          botSettings: {
-            traidingSignals: [],
-            initialOrder: "123",
-            safeOrder: {
-              size: "123",
-              amount: "123"
-            },
-            deviation: 1.23,
-            martingale: {
-              value: 1.01,
-              active: "0"
-            },
-            maxOpenSafetyOrders: "",
-            takeProffit: 1.23,
-            stopLoss: 1.23,
-            dailyVolumeBTC: null
-          },
-          botID: "1534154312886",
-          volumeLimit: [
-            {
-              NAME: "ETH",
-              VALUE: 0.01
-            },
-            {
-              NAME: "BNB",
-              VALUE: 1
-            }
-          ]
-        }
-      ]
+      botsList: []
     },
     getters: {
       getAuthorizedStatus(state) {
@@ -115,16 +59,16 @@ const store = () =>
         commit("addNewBot", payload);
       },
       setBotsList({ commit }) {
-        // this.$axios
-        //   .$get("/bots/getBotsList")
-        //   .then(res => {
-        //     if (res.status === "ok") {
-        //       commit("setBotsList", res.data);
-        //     } else {
-        //       console.log(res.message);
-        //     }
-        //   })
-        //   .catch(e => console.log(e));
+        this.$axios
+          .$get("/bots/getBotsList")
+          .then(res => {
+            if (res.status === "ok") {
+              commit("setBotsList", res.data);
+            } else {
+              console.log(res.message);
+            }
+          })
+          .catch(e => console.log(e));
       },
       deleteBot({ commit }, payload) {
         this.$axios
