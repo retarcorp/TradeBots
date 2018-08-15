cdc4a0c858f3efcb5c82e7be50c0e84d62d76de3
ituxka <ituxka@gmail.com>
2018-08-15 16:41:28 +0300

    upd filtering

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,12 +1,33 @@
 <template>
   <section class="container">
-    
+      <button @click.prevent="onSendMessage" class="button button--primary">SEND WS MESSAGE</button>
   </section>
 </template>
 
 <script>
-
 export default {
+  data() {
+    return {
+      ws: null
+    }
+  },
+  created() {
+    this.ws = new WebSocket('ws://localhost:8072/');
+    this.ws.onopen = () => {
+      console.log("WS подключено");
+    }
+    this.ws.onclose = (eventClose) => {
+      console.log("соеденение закрыто причина:"  + eventclose);
+    }
+    this.ws.onmessage = (msg) => {
+      console.log('Сообщение ' + msg.data)
+    }
+  },
+  methods: {
+    onSendMessage() {
+      this.ws.send('тест сообщение');
+    }
+  }
 }
 </script>
 
