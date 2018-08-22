97d95750a4512ac086ab237911e14a450ef36b78
ituxka <ituxka@gmail.com>
2018-08-22 18:32:22 +0300

    redirect setup

--- frontend/middleware/checkWsStatus.js
+++ frontend/middleware/checkWsStatus.js
@@ -1,9 +1,12 @@
-export default function ({ route, redirect }) {
+export default function ({ store, route, redirect }) {
   if(route.path === '/signin' || route.path === '/signup') {
     console.log('here');
   } else {
     if(!(~document.cookie.indexOf('user'))) {
+      store.commit('setAuthorized', false)
       redirect('/signin')
-    } 
+    } else {
+      store.commit("setAuthorized", true);
+    }
   }
 }
