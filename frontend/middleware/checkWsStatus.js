d92f6a249685fa6dc9e2518452017143d5b011a9
Lafore17 <dimkagomel1@gmail.com>
2018-08-22 22:01:57 +0300

    added refactoring check id in url

--- frontend/middleware/checkWsStatus.js
+++ frontend/middleware/checkWsStatus.js
@@ -1,9 +1,12 @@
-export default function ({ route, redirect }) {
+export default function ({ store, route, redirect }) {
   if(route.path === '/signin' || route.path === '/signup') {
     console.log('here');
   } else {
     if(!(~document.cookie.indexOf('user'))) {
       redirect('/signin')
     } 
+  }
+  if( route.path === `/bots/${route.params.id}` && store.state.botsList.length === 0 ) {
+    redirect('/bots');
   }
 }
