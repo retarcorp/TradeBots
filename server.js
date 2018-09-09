dc5b734a045783584eed9f4585167288c06a85f0
mmfKupl <ikuplevich@mail.ru>
2018-09-09 11:41:02 +0300

    server upd

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ Mongo.init()
 // const Users = require('./modules/Users');
 // const Crypto = require('./modules/Crypto');
 // const Binance = require('./modules/Binance');
-let binanceAPI = require('binance-api-node').default;
+// let binanceAPI = require('binance-api-node').default;	
 
 //Routers
 var signin = require('./routes/signin');
@@ -44,41 +44,15 @@ var nuxt = new Nuxt(options);
 
 app.use(cookieParser());
 app.use(expressSession({secret: 'kitty secret'}));
-//app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.disable('x-powered-by');
 
-
-app.get('/bots/getBotsList', bots)
-app.post('/bots/add', bots)
-app.post('/bots/delete', bots)
-app.post('/bots/update', bots)
-app.post('/bots/setStatus', bots)
-app.post('/bots/orders/cancel', bots)
-app.post('/bots/orders/cancelAll', bots)
-app.post('/bots/setFreeze', bots)
-
-app.get('/account/api', account)
-app.post('/account/api', account)
-app.delete('/account/api', account)
-
-app.get('/signin', signin)
-app.post('/signin', signin)
-app.get('/signout', signin)
-app.post('/signup', signup)
-
-app.get('/bots/getBotsList', bots)
-app.get('/statistics', statistics)
-
-app.get('/test', (req, res, next) => {
-	let c = binanceAPI({
-		apiKey: 'pMK15sHEO3jS9RE9x4KA5zFfdxCKcxk9gDgyf4BhvGrhvEUn3wiZMTYcuqLEAkNh',
-		apiSecret: 'A7pvWxoe0JzHfM1rvF7D2ymM3ZFUvdlOyLRmjeZ7m4gfCWaTOmLBwHcwMUSw3Znp'
-	})
-	c.exchangeInfo().then( data => console.log(data.rateLimits))
-	res.send('lol')
-})
+app.use(bots)
+app.use(account)
+app.use(signin)
+app.use(signup)
+app.use(statistics)
 
 app.use(nuxt.render);
 app.listen(process.env.PORT || 8072);
