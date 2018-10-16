e29a6a1ba046c9266722fe04801cb36e29be93c4
Lafore17 <dimkagomel1@gmail.com>
2018-10-16 10:23:07 +0300

    delete unnecessary code

--- pages/Akaunt/Balance/index.vue
+++ pages/Akaunt/Balance/index.vue
@@ -1,38 +1,6 @@
 <template>
     <div class="balance">
         <div class="binance__sub-title title--small">Для пополнения баланса перечислите средства на кошелек <b>1234567qwer</b></div>
-        <!-- <div class="binance__form">
-            <form class="binance__form">
-                <div class="form-control">
-                    <label class="label">Название</label>
-                    <input v-if="!isSaved" v-model="binance.name" type="text" class="input">
-                    <div v-else>{{ binance.name }}</div>
-                </div>
-                <div class="form-control">
-                    <label class="label">Api Key</label>
-                    <input v-if="!isSaved" v-model="binance.key" type="text" class="input">
-                    <div v-else>{{ binance.key }}</div>
-                </div>
-                <div class="form-control">
-                    <label class="label">Api Secret</label>
-                    <input v-if="!isSaved" v-model="binance.secret" type="text" class="input">
-                    <div v-else>{{ binance.secret ? '***' : '' }}</div>
-                </div>
-                <div class="form__actions">
-                    <div class="d-flex">
-                        <button v-if="!isSaved"
-                                @click.prevent="onSettingsSave" 
-                                :class="{'button--disabled': !isFormValid}" 
-                                :disabled="!isFormValid" 
-                                class="button button--success">Сохранить</button>
-                        <button v-else @click.prevent="isSaved = false" 
-                                class="button button--success">Изменить</button>
-                        <button @click.prevent="onSettingsDelete" 
-                            class="button button--danger">Удалить</button>
-                    </div>
-                </div>
-            </form>
-        </div> -->
     </div>
 </template>
 
@@ -40,33 +8,7 @@
     export default {
         data() {
             return {
-                // binance: {
-                //     name: '',
-                //     key: '',
-                //     secret: ''
-                // },
-                isSaved: true
-            }
-        },
-        computed: {
-            binance() {
-                return this.$store.getters.getBinanceAPI;
-            },
-            isFormValid() {
-                return this.binance.name && this.binance.key && this.binance.secret;
-            }
-        },
-        methods: {
-            onSettingsSave() {
-                // this.$store.commit('setSpiner', true);
-                this.$store.dispatch('setBinanceAPI', this.binance);
-                this.$router.push('/Akaunt');
-            },
-            onSettingsDelete() {
-                this.$store.dispatch('deleteBinanceAPI');
-                this.binance = {};
-                this.isSaved = true;
-                this.$router.push('/Akaunt');
+                
             }
         }
     }
