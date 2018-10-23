623431d27851cb6aafe0691a48c0a3cd5f287d3b
ituxka <ituxka@gmail.com>
2018-10-23 19:54:47 +0300

    upd

---
+++ pages/Admin/Pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <label>Путь</label>
+                <input class="input" v-model="page.slug">
+                <input class="input" v-model="page.title">
+                <input class="input" v-model="page.content">
+                
+                <!-- <vue-ckeditor v-model="page.content" :config="config" /> -->
+                <button @click="onCreatePage">Create</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import VueCkeditor from 'vue-ckeditor2';
+export default {
+    components: { VueCkeditor },
+    head: {
+        script: [
+            {
+                src: "https://cdn.ckeditor.com/ckeditor5/11.1.1/classic/ckeditor.js",
+                src: "https://unpkg.com/vue-ckeditor2"
+            }
+        ]
+    },
+    data() {
+        return {
+            page: {
+                slug: '',
+                title: '',
+                content: ''
+            },
+            config: {
+                toolbar: [
+                    ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript']
+                ],
+                height: 300
+            }
+        }
+    },
+    methods: {
+        onCreatePage() {
+            this.$axios.post('/api/admin/pages/create/', this.page)
+                .then(res => {
+                    console.log(res.data)
+                })
+                .catch(e => console.info(e))
+        }
+    },
+
+    created() {
+        
+    }
+}
+</script>
