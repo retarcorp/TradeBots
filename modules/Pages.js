31c74608f19fae9566141d9ff513eff5431139d7
mmfKupl <ikuplevich@mail.ru>
2018-10-23 19:47:09 +0300

    app endpoint for pages

--- modules/Pages.js
+++ modules/Pages.js
@@ -4,11 +4,65 @@ const { pages } = ('../config/config.js');
 
 class Pages {
 	async createPage(admin = {}, pageData = {}, callback = (data = {}) => {}) {
-		if(await this.authenticationAdmin(admin) && await this.pageExists(pageData)) {
-			Mongo.syncInsert(pageData, pages.collection)
-				.then(result => {
-					callback(M.getSuccessfullyMessage({ data: result }));
-				});
+		if(await this.authenticationAdmin(admin)) {
+			if(await !this.pageExists(pageData)) {
+
+				Mongo.syncInsert(pageData, pages.collection)
+					.then(result => {
+						callback(M.getSuccessfullyMessage({ data: result }));
+					})
+					.catch(err => {
+						console.log(err);
+						callback(M.getFailureMessage({ message: 'ошибка на сервере!', error: err }));
+					});
+
+			} else callback(M.getFailureMessage({ message: 'Такая страница уже существует!' }));
+		} else callback(M.getFailureMessage({ message: 'Недостаточно прав!' }));
+	}
+
+	async getPages(admin = {}, callback = (data = {}) => {}) {
+		if(await this.authenticationAdmin(admin)) {
+			let pages = await Mongo.syncSelect({}, pages.collection);
+			callback(M.getSuccessfullyMessage({ data: pages }));
+		} else callback(M.getFailureMessage({ message: 'Недостаточно прав!' }));
+	}
+
+	async removePage(admin = {}, pageSlug = {}, callback = (data = {}) => {}) {
+		if(await this.authenticationAdmin(admin)) {
+			if(await this.pageExists(pageData)) { 
+
+				Mongo.syncDelete({ slug: pageSlug.slug }, pages.collection)
+					.then(result => {
+						callback(M.getSuccessfullyMessage({ data: result }));
+					})
+					.catch(err => {
+						console.log(err);
+						callback(M.getFailureMessage({ message: 'ошибка на сервере!', error: err }));
+					});
+
+			} else callback(M.getFailureMessage({ message: 'Такая страница не существует!' }));
+		} else callback(M.getFailureMessage({ message: 'Недостаточно прав!' }));
+	}
+
+	async updatePage(admin = {}, nexPageData = {}, callback = (data = {}) => {}) {
+		if(await this.authenticationAdmin(admin)) {
+			if(await this.pageExists(pageData)) { 
+				let key = { slug: nexPageData.slug },
+					change = Object.assign({}, nexPageData);
+
+				change.slug = change.nextSlug;
+				delete change.nextSlug;
+				
+				Mongo.syncUpdate(key, change, pages.collection)
+					.then(result => {
+						callback(M.getSuccessfullyMessage({ page: change, result: result }));
+					})
+					.catch(err => {
+						console.log(err);
+						callback(M.getFailureMessage({ message: 'ошибка на сервере!', error: err }));
+					});
+
+			} else callback(M.getFailureMessage({ message: 'Такая страница не существует!' }));
 		} else callback(M.getFailureMessage({ message: 'Недостаточно прав!' }));
 	}
 
--- routes/pages.js
+++ routes/pages.js
@@ -8,15 +8,18 @@ router.post('/api/admin/pages/create', (req, res, next) => {
 });
 
 router.get('/api/admin/pages/getPages', (req, res, next) => {
-
+	let admin = req.cookies.admin;
+	Pages.getPages(admin, data => res.json(data));
 });
 
 router.post('/api/admin/pages/remove', (req, res, next) => {
-
+	let admin = req.cookies.admin;
+	Pages.removePage(admin, req.body, data => res.json(data));
 });
 
 router.post('/api/admin/pages/update', (req, res, next) => {
-
+	let admin = req.cookies.admin;
+	Pages.updatePage(admin, req.body, data => res.json(data));
 });
 
 module.exports = router;
